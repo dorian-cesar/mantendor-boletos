2beb0cb208f846091b2295596b5620813498ce02
adolfosa <adolfo.sepulveda.astroza@gmail.com>
2025-07-28 16:59:19 -0400

    feat: pagina Servicios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '@components/Login/Login';
 import Dashboard from '@components/Dashboard/Dashboard';
 import Usuarios from '@pages/Usuarios';
+import Servicios from '@pages/Servicios';
 import { ToastContainer } from '@components/Toast/Toast';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/servicios" element={<Servicios />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -3,3 +3,8 @@ import Sidebar from '@components/Sidebar/Sidebar';
 import '@components/Dashboard/dashboard.css';
 import ModalBase from '@components/ModalBase/ModalBase';
 import { showToast } from '@components/Toast/Toast';
+
+const Servicios = () => {  
+};
+
+export default Servicios;
