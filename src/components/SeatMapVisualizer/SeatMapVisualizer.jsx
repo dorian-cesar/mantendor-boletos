8c6149b3e51fdcbaaf0c767afca5b9ffb36956c1
adolfosa <adolfo.sepulveda.astroza@gmail.com>
2025-08-07 16:23:13 -0400

    feat seatMapVisualizer ahora muestra pisos ordenados uno al lado del otro de izquierda a derecha

--- src/components/SeatMapVisualizer/SeatMapVisualizer.jsx
+++ src/components/SeatMapVisualizer/SeatMapVisualizer.jsx
@@ -48,31 +48,32 @@ const SeatMapVisualizer = ({ seatMap, floors }) => {
 
   return (
     <div className="seat-map-container">
-      {floors >= 1 && (
-        <div className="floor-section">
-          <h6>Piso 1</h6>
-          <div className="seat-grid">
-            {seatMap.floor1.seatMap.map((row, rowIdx) => (
-              <div key={`f1r${rowIdx}`} className="seat-row">
-                {renderSeatRow(row, rowIdx, 'f1')}
-              </div>
-            ))}
+      <div className="floors-row">
+        {floors >= 1 && (
+          <div className="floor-section">
+            <h6>Piso 1</h6>
+            <div className="seat-grid">
+              {seatMap.floor1.seatMap.map((row, rowIdx) => (
+                <div key={`f1r${rowIdx}`} className="seat-row">
+                  {renderSeatRow(row, rowIdx, 'f1')}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-      
-      {floors >= 2 && seatMap.floor2.seatMap.length > 0 && (
-        <div className="floor-section mt-3">
-          <h6>Piso 2</h6>
-          <div className="seat-grid">
-            {seatMap.floor2.seatMap.map((row, rowIdx) => (
-              <div key={`f2r${rowIdx}`} className="seat-row">
-                {renderSeatRow(row, rowIdx, 'f2')}
-              </div>
-            ))}
+        )}
+        {floors >= 2 && seatMap.floor2.seatMap.length > 0 && (
+          <div className="floor-section">
+            <h6>Piso 2</h6>
+            <div className="seat-grid">
+              {seatMap.floor2.seatMap.map((row, rowIdx) => (
+                <div key={`f2r${rowIdx}`} className="seat-row">
+                  {renderSeatRow(row, rowIdx, 'f2')}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
